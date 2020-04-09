75ccca7e04fc80f6f3f8eef14635a064dd772d43
RicardoCounago <ciricardo94@gmail.com>
2020-04-09 20:47:53 +0200

    Separación de componentes

---
+++ ex00/src/components/MyApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyApp() {
+    return(
+        <div>
+            <h1>Ricardo</h1>
+            <p>Un párrafo con texto</p>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
+}
+
+export default MyApp;
--- ex00/src/index.js
+++ ex00/src/index.js
@@ -1,4 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello world!</h1>,document.getElementById("root"))
+import MyApp from './components/MyApp'
+
+ReactDOM.render(<MyApp/>,document.getElementById('root'))
