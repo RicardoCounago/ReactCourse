5521c0f1480dd962046900691df12b4debbba540
RicardoCounago <ciricardo94@gmail.com>
2020-04-10 12:13:41 +0200

    Parent/Child components

---
+++ ex00/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+    return(
+        <main>
+            <p>This is where the rest of the things go...</p>
+        </main>
+    )
+}
+
+export default Main;
---
+++ ex00/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+    return(
+        <nav>
+            <h1>Hello</h1>
+            <ul>
+                <li>Tging 1</li>
+                <li>Tging 2</li>
+                <li>Tging 3</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
